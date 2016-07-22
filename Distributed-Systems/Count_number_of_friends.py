299f2cb5bd87b4b3a56afced194c2a9ad56b17a7
Guanyu Wang <guanyuwang.cs@gmail.com>
2016-07-22 12:58:58 -0400

    Create Count_number_of_friends.py

---
+++ Distributed-Systems/Count_number_of_friends.py
@@ -0,0 +1,46 @@
+import sys
+from collections import OrderedDict
+class MapReduce:
+    def __init__(self):
+        self.intermediate = OrderedDict()
+        self.result = []
+   
+
+    def emitIntermediate(self, key, value):
+        self.intermediate.setdefault(key, [])       
+        self.intermediate[key].append(value)
+
+    def emit(self, value):
+        self.result.append(value) 
+
+    def execute(self, data, mapper, reducer):
+        for record in data:
+            mapper(record)
+
+        for key in self.intermediate:
+            reducer(key, self.intermediate[key])
+
+        self.result.sort()
+        for item in self.result:
+            print "{\"key\":\""+item[0]+"\",\"value\":\"" + str(item[1]) + "\"}"
+
+mapReducer = MapReduce()
+
+def mapper(record):
+    #Start writing the Map code here
+    users = record.split()
+    mapReducer.emitIntermediate(users[0], 1)
+    mapReducer.emitIntermediate(users[1], 1)
+
+def reducer(key, list_of_values):
+    #Start writing the Reduce code here
+    ret = 0
+    for x in list_of_values:
+        ret += x
+    mapReducer.emit([key, ret])
+    
+if __name__ == '__main__':
+    inputData = []
+    for line in sys.stdin:
+        inputData.append(line)
+    mapReducer.execute(inputData, mapper, reducer)
