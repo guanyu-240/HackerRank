512012dd3af6f699837ebf55c238a62904232e38
Guanyu Wang <guanyuwang.cs@gmail.com>
2017-01-14 09:42:40 -0500

    Create LinkedLists-Merge_Two_Lists.py

---
+++ Data-Structures/LinkedLists-Merge_Two_Lists.py
@@ -0,0 +1,22 @@
+"""
+ Merge two linked lists
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+def MergeLists(headA, headB):
+    if not headA: return headB
+    elif not headB: return headA
+    elif headA.data < headB.data: 
+        headA.next = MergeLists(headA.next, headB)
+        return headA
+    else:
+        headB.next = MergeLists(headA, headB.next)
+        return headB
